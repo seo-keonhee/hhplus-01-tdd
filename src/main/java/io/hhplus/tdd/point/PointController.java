1d66412f4e0a25b88d6cfd4a9840762035948afc
seo-keonhee <sluckhy17@gmail.com>
2024-06-21 05:52:01 +0900

    1주차 TDD로 개발하기 1차

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import io.hhplus.tdd.database.*;
+import io.hhplus.tdd.point.service.*;
+
+// 나눠보려고 했는데 결국 로직이 다 컨트롤러에 몰린것 같아요 ㅠㅠ
+// 서비스단에서 처리했어야 할 로직을 알려주시면 감사합니다.
 
 @RestController
 @RequestMapping("/point")
@@ -21,10 +24,9 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        // 유저포인트 테이블 객체 생성
-        UserPointTable userPointTable = new UserPointTable();
-        // 유저포인트 조회
-        return userPointTable.selectById(id);
+        // 포인트서비스 객체 생성
+        PointService pointService = new PointService();
+        return pointService.findPointById(id);
     }
 
     /**
@@ -34,47 +36,51 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        // 포인트 사용이력 테이블 객체생성
-        PointHistoryTable pointHistoryTable = new PointHistoryTable();
-        // 포인트 사용이력 테이블 조회
-        return pointHistoryTable.selectAllByUserId(id);
+        // 포인트이력 서비스 객체생성
+        HistoryService historyService = new HistoryService();
+        return historyService.findAllById(id);
     }
+
 
     /**
      * 특정 유저의 포인트를 충전하는 기능
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(
+    public synchronized UserPoint charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        // 유저포인트, 포인트 사용이력 테이블 객체 생성
-        UserPointTable userPointTable = new UserPointTable();
-        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+        // 포인트서비스, 포인트이력서비스 객체 생성
+        PointService pointService = new PointService();
+        HistoryService historyService = new HistoryService();
         // 유저포인트 조회
-        UserPoint uPoint = userPointTable.selectById(id);
-        // 포인트 사용이력 추가
-        pointHistoryTable.insert(id, uPoint.point() + amount, TransactionType.CHARGE, System.currentTimeMillis());
+        long point = pointService.findPointById(id).point();
+        // 적립이력 추가
+        historyService.insertHistory(id, point + amount, TransactionType.CHARGE, System.currentTimeMillis());
         // 유저포인트 적립
-        return userPointTable.insertOrUpdate(id, uPoint.point() + amount);
+        return pointService.updatePointById(id, point + amount);
     }
     
     /**
      * 특정 유저의 포인트를 사용하는 기능
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(
+    public synchronized UserPoint use(
             @PathVariable long id,
             @RequestBody long amount
-    ) {
-        // 유저포인트, 포인트 사용이력 테이블 객체 생성
-        UserPointTable userPointTable = new UserPointTable();
-        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+    ) throws Exception {
+        // 포인트서비스, 포인트이력서비스 객체 생성
+        PointService pointService = new PointService();
+        HistoryService historyService = new HistoryService();
         // 유저포인트 조회
-        UserPoint uPoint = userPointTable.selectById(id);
-        // 포인트 사용이력 추가
-        pointHistoryTable.insert(id, uPoint.point() - amount, TransactionType.USE, System.currentTimeMillis());
+        long point = pointService.findPointById(id).point();
+        // 소지한 포인트 보다 적을 경우 반환
+        if (point < amount) {
+            throw new Exception("소지한 포인트가 부족합니다!!");
+        }
+        // 사용이력 추가
+        historyService.insertHistory(id, point - amount, TransactionType.USE, System.currentTimeMillis());
         // 유저포인트 사용
-        return userPointTable.insertOrUpdate(id, uPoint.point() - amount);
+        return pointService.updatePointById(id, point - amount);
     }
 }
